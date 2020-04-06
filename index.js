7f4af3cb7e514f0e6e329e004566eb5d2e897c75
rafacartagena <rcp77@alu.ua.es>
2020-04-06 19:14:59 +0200

    Mi primer API REST inútil

--- index.js
+++ index.js
@@ -3,13 +3,36 @@
 const port = process.env.PORT || 3000;
 
 const express = require('express');
+const logger = require('morgan');
+
 const app = express();
 
-app.get('/hola/:unNombre', (request, response) => {
-    // response.send(`Hola a ${request.params.unNombre} desde Express`);
-    response.status(200).send({mensaje:`Hola ${request.params.unNombre} desde API con JSON`});
+//Middleware
+app.use(logger('dev')); //tiny, short, dev, common, combined
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//API REST
+app.get('/api/product', (req, res) => {
+    res.status(200).send({'lista de productos':[]});
+});
+
+app.get('/api/product/:productID', (req, res) => {
+    res.status(200).send({producto: `${req.params.productID}`});
+});
+
+app.post('/api/product', (req, res) => {
+    console.log(req.body);
+    // Crearía el nuevo objeto
+    res.status(200).send({producto: 'El producto se ha creado'});
+});
+
+app.put('/api/product:productID', (req, res) => {
+    console.log(req.body);
+    // Crearía el nuevo objeto
+    res.status(200).send({producto: 'El producto se ha creado'});
 });
 
 app.listen(port, () =>{
-    console.log(`API REST ejecutándose en http://localhost:${port}/hola/:tuNombre`);
+    console.log(`API REST ejecutándose en http://localhost:${port}/api/product`);
 });

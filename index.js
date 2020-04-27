94d392f65937e31a30fba0ce6b7de494918097dc
rafacartagena <rcp77@alu.ua.es>
2020-04-27 16:05:07 +0200

    API REST CRUD con MongoDB

--- index.js
+++ index.js
@@ -1,38 +1,92 @@
 'use strict'
 
+// API REST
+//  /api/:coleccion/:id
+
 const port = process.env.PORT || 3000;
 
 const express = require('express');
+const mongojs = require('mongojs');
 const logger = require('morgan');
 
-const app = express();
+//Conectamos con la BD
+var db = mongojs("API");
+var id = mongojs.ObjectID; //método para convertir un id textual en un objetio mongo
+
+//Creamos la aplicación o servicio Express
+const app = express(); 
 
 //Middleware
 app.use(logger('dev')); //tiny, short, dev, common, combined
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+//Añadimos un trigger previo a las rutas para dar soporte a múltiples colecciones
+app.param("coleccion", (req, res, next, coleccion) => {
+    console.log('param /api/:coleccion'); 
+    console.log(`colección: ${colección}`); 
+
+    req.collection = db.collection(coleccion);
+    return next();
+});
+
+
 //API REST
-app.get('/api/product', (req, res) => {
-    res.status(200).send({'lista de productos':[]});
+
+//Routes
+ app.get('/api', (req, res, next) => {
+    console.log(req.params);
+    console.log(req.collection);
+
+    db.getCollectionNames((err, colecciones) => {
+        if (err) return next(err);
+        res.json(colecciones);
+    });
+ }); 
+    
+app.get('/api/:coleccion', (req, res, next) => {
+    req.collection.find((err, coleccion) => {
+        if (err) return next(err);
+        res.json(coleccion);
+    });
 });
 
-app.get('/api/product/:productID', (req, res) => {
-    res.status(200).send({producto: `${req.params.productID}`});
+app.get('/api/:coleccion/:id', (req, res, next) => {
+    req.collection.findOne({_id: id(req.params.id)}, (err, item) => {
+        if (err) return next(err);
+        res.json(item);
+    });
 });
 
-app.post('/api/product', (req, res) => {
-    console.log(req.body);
-    // Crearía el nuevo objeto
-    res.status(200).send({producto: 'El producto se ha creado'});
+app.post('/api/:coleccion', (req, res, next) => {
+    const elemento = req.body;
+
+    console.log(elemento);
+
+    req.collection.save(elemento, (err, coleccionGuardada) => {
+        if (err) return next(err);
+        res.json(coleccionGuardada);
+    });
 });
 
-app.put('/api/product:productID', (req, res) => {
-    console.log(req.body);
-    // Crearía el nuevo objeto
-    res.status(200).send({producto: 'El producto se ha creado'});
+app.put('/api/:coleccion/:id', (res, req, next) => {
+    var elementoID = req.params.id;
+    var elementoNuevo = req.body;
+
+    req.collection.update({_id: id(elementoID)}, {&set: elementoNuevo}, {safe: true, multi: false}, (err, elementoModif) => {
+        if (err) return next(err);
+        res.json(elementoModif);
+    });
 });
 
+app.delete('/api/:coleccion/:id', (req, res, next) => {
+    req.collection.remove({_id: id(req.params.id)}, (err, result) => {
+        if (err) return next(err);
+        res.json(result);
+    });
+});
+
+//Iniciamos el servicio CRUD
 app.listen(port, () =>{
-    console.log(`API REST ejecutándose en http://localhost:${port}/api/product`);
+    console.log(`API REST ejecutándose en http://localhost:${port}/api/:coleccion:id`);
 });
